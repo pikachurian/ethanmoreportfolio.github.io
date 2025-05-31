71bb07ba8f2e23c1c030be30c828281847d3e680
pikachurian <111457121+pikachurian@users.noreply.github.com>
2025-05-31 11:45:37 -0400

    Edited Astro Config Json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "ethanmoreportfolio.github.io",
-    
+    site: 'https://pikachurian.github.io',
+    base: 'ethanmoreportfolio.github.io',
 });
